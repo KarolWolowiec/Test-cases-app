101f4cc47878877d626f8ca37bc1743606792a1a
Karol <wolowiec.karol@protonmail.com>
2023-01-19 17:44:17 +0100

    added 'Category' and 'TestCase' classes

---
+++ Test/Data/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
+
+        public List<TestCase> TestCases { get; set; } = new()
+    }
+}
---
+++ Test/Data/Models/TestCase.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Data.Models
+{
+    public class TestCase
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Title { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Required]
+        public int? CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
