d8e4ebfcc589110c87c08f3a1f59b3677016246f
Karol <wolowiec.karol@protonmail.com>
2023-01-20 13:25:58 +0100

    filled TestCasesManagerDb with some test data

--- Test/Data/TestCasesManagerDbContext.cs
+++ Test/Data/TestCasesManagerDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Test.Data.Models;
 
 namespace Test.Data
@@ -10,5 +11,23 @@ namespace Test.Data
         public DbSet<TestCase> TestCases => Set<TestCase>();
         public DbSet<Category> Categories => Set<Category>();
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "User interface" },
+                new Category { Id = 2, Name = "API" },
+                new Category { Id = 3, Name = "Documentation" },
+                new Category { Id = 4, Name = "Performance" },
+                new Category { Id = 5, Name = "Other" });
+
+            modelBuilder.Entity<TestCase>().HasData(
+                new TestCase { Id = 1, Title = "Simple title", Description = "A test description", CategoryId = 1 },
+                new TestCase { Id = 2, Title = "Simple title 2", Description = "A test description 2", CategoryId = 2 },
+                new TestCase { Id = 3, Title = "Simple title 3", Description = "A test description 3", CategoryId = 3 },
+                new TestCase { Id = 4, Title = "Simple title 4", Description = "A test description 4", CategoryId = 4 },
+                new TestCase { Id = 5, Title = "Simple title 5", Description = "A test description 5", CategoryId = 5 });
+        }
     }
 }
---
+++ Test/Migrations/20230120122303_SeedDatabase.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Test.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "User interface" },
+                    { 2, "API" },
+                    { 3, "Documentation" },
+                    { 4, "Performance" },
+                    { 5, "Other" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TestCases",
+                columns: new[] { "Id", "CategoryId", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "A test description", "Simple title" },
+                    { 2, 2, "A test description 2", "Simple title 2" },
+                    { 3, 3, "A test description 3", "Simple title 3" },
+                    { 4, 4, "A test description 4", "Simple title 4" },
+                    { 5, 5, "A test description 5", "Simple title 5" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TestCases",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TestCases",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TestCases",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TestCases",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "TestCases",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
